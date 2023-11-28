583df8f6b0527cf5739f38189552aa0fcf89bfb6
jsochoaco <sebastianochoa05@gmail.com>
2023-11-28 14:13:15 -0500

    Port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const server = require('./src/server.js');
 const { conn } = require('./src/db.js');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 
 conn.sync({ force: false }).then(() => {
