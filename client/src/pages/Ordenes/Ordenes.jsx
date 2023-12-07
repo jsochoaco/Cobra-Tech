c4c28d03e146fb563f72a74e105608db4275e315
jsochoaco <sebastianochoa05@gmail.com>
2023-12-06 21:29:27 -0500

    Visualización de ordenes

--- client/src/pages/Ordenes/Ordenes.jsx
+++ client/src/pages/Ordenes/Ordenes.jsx
@@ -9,6 +9,10 @@ import * as actions from "../../redux/actions"
 const Ordenes = () => {
     const dispatch = useDispatch()
     const orders = useSelector((state) => state.allOrdenes)
+    const sortedOrders = [...orders].sort((ordenA, ordenB) => {
+        // Comparar los IDs de orden de manera ascendente
+        return ordenB.id - ordenA.id;
+      });
     useEffect(()=> {
         dispatch(actions.getAllOrders())
       }, [dispatch])
@@ -16,12 +20,21 @@ const Ordenes = () => {
     const porPagina = 8
     const ultimoElemento = pagina*porPagina
     const primerElemento = ultimoElemento - porPagina
-    const actualOrders = orders.slice(primerElemento, ultimoElemento)
-    const totalPages = Math.ceil(orders.length / porPagina);
+    const actualOrders = sortedOrders.slice(primerElemento, ultimoElemento)
+    const totalPages = Math.ceil(sortedOrders.length / porPagina);
     const pageNumbers = [];
     for (let i = 1; i <= totalPages; i++) {
         pageNumbers.push(i);}
     useEffect(() => {setPagina(1)}, [totalPages]);
+    const maxPaginasAMostrar = 5; // Número máximo de páginas que se mostrarán en la barra de paginación
+    const inicioPaginas =
+    pagina <= Math.ceil(maxPaginasAMostrar / 2)
+    ? 1
+    : Math.max(1, pagina - Math.floor(maxPaginasAMostrar / 2))
+    const finPaginas = Math.min(
+        inicioPaginas + maxPaginasAMostrar - 1,
+        totalPages
+);
     return (
         <div className={style.divContenedor}>
             <NavBar/>
@@ -46,11 +59,30 @@ const Ordenes = () => {
                     </table>
                     {orders.length > 8 && 
                     <div className={style.paginado}>
-                        <button className= {style.botonpag} onClick={() => setPagina(pagina - 1)} disabled={pagina === 1}> Anterior </button>
-                        {pageNumbers.map((pageNumber) => (
-                        <button key={pageNumber} className={pageNumber === pagina ? style.pagina : style.paginaboton} onClick={() => setPagina(pageNumber)}>{pageNumber}</button>))}
-                        <button className= {style.botonpag} onClick={() => setPagina(pagina + 1)} disabled={ultimoElemento >= orders.length}>Siguiente</button>
-                    </div>}
+                    <button
+                      className={style.botonpag}
+                      onClick={() => setPagina(pagina - 1)}
+                      disabled={pagina === 1}
+                    >
+                      Anterior
+                    </button>
+                    {pageNumbers.slice(inicioPaginas - 1, finPaginas).map((pageNumber) => (
+                      <button
+                        key={pageNumber}
+                        className={pageNumber === pagina ? style.pagina : style.paginaboton}
+                        onClick={() => setPagina(pageNumber)}
+                      >
+                        {pageNumber}
+                      </button>
+                    ))}
+                    <button
+                      className={style.botonpag}
+                      onClick={() => setPagina(pagina + 1)}
+                      disabled={ultimoElemento >= orders.length}
+                    >
+                      Siguiente
+                    </button>
+                  </div>}
                 </div>
             </div>
         </div>
