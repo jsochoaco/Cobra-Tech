d9097b10030b992993b4c2f57a0cfd02f7fe780e
jsochoaco <sebastianochoa05@gmail.com>
2023-12-13 13:55:29 -0500

    Middle

--- server/src/server.js
+++ server/src/server.js
@@ -3,10 +3,24 @@ const morgan = require("morgan");
 const cors = require("cors");
 const routers = require("./routes/"); // Asegúrate de que la ruta a tus rutas sea correcta.
 const server = express();
+
 //===MIDDLEWARES
 server.use(morgan("dev")); // Morgan con el formato "dev" para registrar las solicitudes en la consola.
 server.use(express.json()); // Express para analizar las solicitudes entrantes con formato JSON.
 server.use(cors()); // Cors para permitir solicitudes de diferentes dominios.
+
+// Middleware para verificar la API key en parámetros de consulta
+const apiKeyMiddleware = (req, res, next) => {
+  const apiKey = req.query.apiKey; // asume que la clave está en los parámetros de consulta
+  // Verifica la existencia y validez de la API key
+  if (apiKey && apiKey === 'cobra2023') {
+    next(); // Clave válida, permite la solicitud
+  } else {
+    res.status(401).json({ error: 'Acceso no autorizado' });
+  }
+};
+
+server.use(apiKeyMiddleware); // Aplicar middleware antes de las rutas
 server.use(routers); // Módulo de rutas importado para manejar las rutas y las solicitudes en la aplicación.
 
-module.exports = server; // Exportamos la instancia de la aplicación
+module.exports = server; // Exportamos la instancia de la aplicación aquí
